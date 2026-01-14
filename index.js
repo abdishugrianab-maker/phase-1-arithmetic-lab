c768f8afab2dd0d708ce785e5eb38e4aa666beec
anab ali <abdishugrianab@gmail.com>
2026-01-14 11:40:43 +0300

    complete

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
+// multiply: equation whose result equals 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// random: random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1;
+
+// mod: equation whose remainder equals 4
+const num3 = 14;
+const num4 = 5;
+const mod = num3 % num4;
+
+// max: highest number should be 20
+const max = Math.max(1, 5, 10, 20);
+
